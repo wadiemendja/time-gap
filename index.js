4e29bd77555f437c6bc020424c078a0bac0828c5
Wadie Mendja <mendja2014@gmail.com>
2021-08-08 18:50:49 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+function calculateTimeGap(time) {
+    const currenTime = new Date();
+    const postTime = new Date(time);
+    const timeDef = currenTime.getTime() - postTime.getTime();
+    const minutesGap = parseInt(timeDef / (1000 * 60));
+    const hoursGap = parseInt(timeDef / (1000 * 60 * 60));
+    const daysGap = parseInt(timeDef / (1000 * 60 * 60 * 24));
+    const monthsGap = parseInt(timeDef / (1000 * 60 * 60 * 24 * 30));
+    const yearsGap = parseInt(timeDef / (1000 * 60 * 60 * 24 * 30 * 12));
+    if (yearsGap > 0) {
+        if (yearsGap == 1) return "1 year ago";
+        else return yearsGap + " years ago";
+    } else if (monthsGap == 1) return "1 month ago";
+    else if (monthsGap > 1) return monthsGap + " months ago";
+    else if (monthsGap == 0) {
+        if (daysGap == 0) {
+            if (hoursGap == 0) {
+                if (minutesGap == 0) return "Just now";
+                else if (minutesGap == 1) return "1 minute ago"
+                else return minutesGap + " minutes ago";
+            } else if (hoursGap == 1) return "1 hour ago";
+            else return hoursGap + " hours ago";
+        } else if (daysGap < 7) {
+            if (daysGap == 1) return "1 day ago";
+            else return daysGap + " days ago";
+        } else if (daysGap >= 7 && daysGap < 14) return "1 week ago";
+        else if (daysGap >= 14 && daysGap < 21) return "2 weeks ago";
+        else return "3 weeks ago";
+    }
+}
